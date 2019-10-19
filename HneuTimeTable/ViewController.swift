82be963bda2b5f34511999b615d234258fc9d138
Kirill <tarakakuc@gmail.com>
2019-10-19 20:19:22 +0300

    Initial Commit

---
+++ HneuTimeTable/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  HneuTimeTable
+//
+//  Created by Kirill on 19.10.2019.
+//  Copyright © 2019 Kirill. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
